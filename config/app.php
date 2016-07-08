4cb48b4f47ed885a8da3748c6a28ee5e689bb3f7
liying <ying.li07@billionsfinance.cn>
2016-07-08 12:24:25 +0800

    add bican/roles

--- config/app.php
+++ config/app.php
@@ -158,6 +158,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Bican\Roles\RolesServiceProvider::class,
     ],
 
     /*
